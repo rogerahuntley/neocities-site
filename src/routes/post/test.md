2e93f327863e0440d4079b8daaf50a468a51ea2b
Roger Huntley II <roger.a.huntley@gmail.com>
2022-05-29 13:35:59 -0400

    add markdown

---
+++ src/routes/post/test.md
@@ -0,0 +1,6 @@
+---
+title: test post
+date: "2021-5-29"
+---
+
+This is a test post!
--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,43 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import sveltePreprocess from 'svelte-preprocess';
+import { resolve } from 'path';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-    scss: {
-      prependData: `
-        @import './src/styles/variables.scss';
-        @import 'include-media/dist/_include-media.scss';`
-    }
-  }),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    sveltePreprocess({
+      scss: {
+        prependData: `
+          @import './src/styles/variables.scss';
+          @import 'include-media/dist/_include-media.scss';
+        `
+      }
+    }),
+    mdsvex({
+      extensions: ['.md']
+    })
+  ],
+  
+  extensions: ['.svelte', '.md'],
 
-	kit: {
-		adapter: adapter(),
-		prerender: {
-			default: true
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      default: true
+    },
+    vite: {
+      resolve: {
+        alias: {
+          '@': resolve('./src'),
+          '$stores': resolve('./src/stores'),
+          '$types': resolve('./src/types'),
+        }
+      }
+    }
+  }
 };
 
 export default config;
