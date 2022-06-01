10cc8cf8a0df0bfdd280de57ea9550a8f26da932
Roger Huntley <rogerh@hesonline.com>
2022-06-01 12:29:51 -0400

    add .vscode to gitignore

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import sveltePreprocess from 'svelte-preprocess';
 import { resolve } from 'path';
 import { mdsvex } from 'mdsvex';
-import autoprefixer from 'autoprefixer'
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
