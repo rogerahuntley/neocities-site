d7cf9666e75a41c3f2d38dc7cfb4da907d274e88
Roger Huntley <rogerh@hesonline.com>
2022-06-01 13:03:40 -0400

    css updates, kassy fix

--- src/routes/__layout@root.svelte
+++ src/routes/__layout@root.svelte
@@ -1,26 +1,37 @@
-<header>
-  roger
-</header>
+<div id="grid-main">
+  <header>
+    roger
+  </header>
 
-<nav>
-  <ul>
-    <li>
-      <a href="/">home</a>
-    </li>
-    <li>
-      <a href="/posts">journal</a>
-    </li>
-  </ul>
-</nav>
+  <nav>
+    <ul>
+      <li>
+        <a href="/">home</a>
+      </li>
+      <li>
+        <a href="/posts">journal</a>
+      </li>
+    </ul>
+  </nav>
 
-<aside>
-  boop!
-</aside>
+  <aside>
+    boop!
+  </aside>
 
-<main>
-  <slot />
-</main>
+  <main>
+    <slot />
+  </main>
 
-<footer>
-  huntley
-</footer>
+  <footer>
+    huntley
+  </footer>
+</div>
+
+<style lang="scss">
+  @import 'src/styles/grid-main.scss';
+  :global(body) {
+    background: url("/images/train.jpg");
+    background-position: center;
+    background-size: cover;
+  }
+</style>
