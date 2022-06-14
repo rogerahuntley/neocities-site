10937186895e65c772b21e4505d77285e7dfa2cb
Roger Huntley II <roger.a.huntley@gmail.com>
2022-06-13 21:47:44 -0400

    add not found

---
+++ src/routes/not_found.svelte
@@ -0,0 +1,7 @@
+<script>
+	import * as Lists from '$lib/lists/index';
+	import { title } from '$stores/title.store';
+	title.set('Eorror');
+</script>
+
+<p>Oops! Looking for <a href="/">this</a>?</p>
