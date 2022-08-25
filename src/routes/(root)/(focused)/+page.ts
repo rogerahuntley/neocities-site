e6b22078de5194c3ce1a53677fb459006ada5d14
Roger Huntley <rogerh@hesonline.com>
2022-08-24 22:12:49 -0400

    remove console log

--- src/routes/(root)/(focused)/+page.ts
+++ src/routes/(root)/(focused)/+page.ts
@@ -4,7 +4,6 @@ export const load = async ({ fetch }) => {
   const allJournals = await getPostsByFilter({ type: 'journal' });
   const sitedata = await fetch('/api/neocities.json');
   const sitedataJson = await sitedata.json();
-  console.log(sitedataJson);
 
   return {
     journals: allJournals,
